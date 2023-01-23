3d5356232a1ac3265a0eb1c88e0b26ef5cc458c8
Shivam <shivamtech899@gmail.com>
2023-01-23 13:44:58 +0530

    4 files are added successfully.

---
+++ src/A1.java
@@ -0,0 +1,11 @@
+public class A1
+{
+
+    public static void main(String args[])
+    {
+        if(true)
+            System.out.println("Hai");
+        else
+            System.out.println("Hello");
+    }
+}
---
+++ src/Single_linked_list.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.Node;
+
+public class Single_linked_list {
+    private Node head;
+
+    private static class Node
+    {
+        private int data;
+        private Node next;
+
+        public Node (int data)
+        {
+            this.data= Integer.parseInt(null);
+            this.next= null;
+
+        }
+    }
+}
